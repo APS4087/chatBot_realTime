23f097b6c58822255e1cf4d764f034b525d39be7
APS4087 <aungpyaesoe.bgs@Gmail.com>
2024-02-23 15:34:47 +0800

    Added configs and db connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,41 @@
-const express = require("express")
+// importing 
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./DB/connectToMongoDB.js";
 
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+// main page route
 app.get("/",(req, res)=>{
     // root route 
     res.send("TESTING RESTART !!!")
 });
 
-app.listen(5000, ()=> console.log('Server Running at port 5000'));
+/* // sign up route
+app.get("/api/auth/signup", (req, res)=> {
+    console.log("signup route");
+});
+
+// login route
+app.get("/api/auth/login", (req, res)=> {
+    console.log("Login route");
+});
+
+// logout route
+app.get("/api/auth/logout", (req, res)=> {
+    console.log("Logout route");
+}); */
+
+// instead of the above, making code nicer with express 
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, ()=> {
+
+    connectToMongoDB();
+    console.log(`Server Running at port ${PORT}`);
+
+});
