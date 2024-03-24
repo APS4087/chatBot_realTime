a4b83517a17872edccfd4e71fb2c4ff7fada4799
APS4087 <aungpyaesoe.bgs@Gmail.com>
2024-03-24 20:08:56 +0800

    updates on messaging frontend

--- frontend/src/components/sideBar/SideBar.jsx
+++ frontend/src/components/sideBar/SideBar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Search from './Search'
+import Conversations from './Conversations'
+import LogoutBtn from './LogoutBtn'
 
 const SideBar = () => {
   return (
-    <div>
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
         <Search />
         <div className='divider px-3'></div>
+        <Conversations />
+        <LogoutBtn />
     </div>
+    
   )
 }
 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import SideBar from '../../components/sideBar/SideBar'
+import MessageContainer from '../../components/messageSide/MessageContainer'
 
 const Home = () => {
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-green-300 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <SideBar / >
-      {/*<MessageSide/>*/}
+      <MessageContainer />
     </div>
-  )
-}
+  );
+};
 
 export default Home
